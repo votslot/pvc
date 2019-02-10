45a909a844f694855ed373e4741960f615cec243
votslot <mpolotskaka64@gmail.com>
2019-02-10 12:57:09 -0800

    add missing inc file

---
+++ shaders/ginclude.glsl
@@ -0,0 +1,32 @@
+
+
+#ifndef _G_INCLUDE_GLSL
+#define _G_INCLUDE_GLSL
+
+const std::string cs_glversion = 
+R""(
+	#version 430 core 
+)"";
+
+const std::string  cs_structs = 
+R""(
+	struct  GlobalParams
+	{
+		float screenX;
+		float screenY;
+		float zNear;
+		float zFar;
+		float zScale;
+	};
+
+	struct Partition
+	{
+		uint first;
+		uint last;
+		float sz;
+		float pad;
+	};
+)"";
+
+#endif
+
